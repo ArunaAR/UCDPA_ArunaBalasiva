e0bfe70b457769ac728ce8262f53759caba91353
aruna <aruna.snafu@gmail.com>
2022-08-15 12:10:50 +0100

    Second commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-
 # import re
 
 pd.set_option('display.max_columns', 500)
@@ -11,10 +10,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import collections
 import string
-
 import nltk
-from nltk.corpus import stopwords
 
+from nltk.corpus import stopwords
 nltk.download('stopwords')
 
 import plotly.express as px
@@ -34,23 +32,32 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_score
 from sklearn.metrics import classification_report
 
+#Data Import, Preprocessing
 spamham_data = pd.read_csv(r"C:\Users\aruna\OneDrive\Desktop\UCDPA_Assignment\spam.csv", encoding="ISO-8859-1")
 print(spamham_data.head(5))
 
+# number of rows and columns in this dataset
 print(spamham_data.shape)
 
+# print columns names and dataType.There's no null value in this dataset
 print(spamham_data.info())
 
+# removed Column that start with U
 spamham_data = spamham_data.loc[:, ~spamham_data.columns.str.contains('^U')]
 print(spamham_data.head(5))
 
+#Rename Column V1 and V2
 spamham_data = spamham_data.rename(
     columns={spamham_data.columns[0]: 'catergories', spamham_data.columns[1]: 'messages'})
 print(spamham_data.head(5))
 
+#Checking if Null values presents in the table
 print(spamham_data.isnull().sum())
+
+#Total SPAM and HAM
 print(spamham_data['catergories'].value_counts())
 
+#Adding new column Length to the table
 spamham_data['Length'] = 0
 for x in np.arange(0, len(spamham_data.messages)):
     spamham_data.loc[x, 'Length'] = len(spamham_data.loc[x, 'messages'])
@@ -63,38 +70,41 @@ print("Not SPAM email Ratio catergory-HAM :",
 print("Spam Email Ratio catergory-SPAM:",
       round(len(spamham_data[spamham_data['catergories'] == 'spam']) / len(spamham_data['catergories']), 2) * 100, "%")
 
-# plt.figure(figsize=(10,10))
-# sns.barplot(x=spamham_data['catergories'].value_counts().index,y=spamham_data['catergories'].value_counts(),data=spamham_data)
-# plt.title("Number of SMS based on Catergories")
-# #plt.show()
+#Bar based on Catergories
+plt.figure(figsize=(10,10))
+sns.barplot(x=spamham_data['catergories'].value_counts().index,y=spamham_data['catergories'].value_counts(),data=spamham_data)
+plt.title("Number of SMS based on Catergories")
+plt.show()
 
+#Histogram
+spamham_data.hist(column='Length',by='catergories',bins=70,figsize=(15,5));
+plt.xlim(-40,950);
+plt.show()
 
-# spamham_data.hist(column='Length',by='catergories',bins=70,figsize=(15,5));
-# plt.xlim(-40,950);
-# plt.show()
-
+#Adding new column no_of_words to the table and converting all the text to lowercase
 spamham_data['no_of_words'] = spamham_data['messages'].apply(lambda x: len(nltk.word_tokenize(x)))
 spamham_data['messages'] = spamham_data['messages'].apply(lambda x: x.lower())
 print(spamham_data.head(5))
 
+#Scatter Graph based on Length and No of Words
+fig = px.scatter_matrix(spamham_data, dimensions=["Length",'no_of_words'],
+                        color = "catergories",template='gridon',
+                        color_discrete_map = {'ham': 'forestgreen', 'spam': 'red'},
+                        title = "SPAM and HAM ")
+#fig.update_traces(diagonal_visible=False)
+fig.show()
 
-# fig = px.scatter_matrix(spamham_data, dimensions=["Length",'no_of_words'],
-#                         color = "catergories",template='gridon',
-#                         color_discrete_map = {'ham': 'forestgreen', 'spam': 'red'},
-#                         title = "SPAM and HAM ")
-# #fig.update_traces(diagonal_visible=False)
-# fig.show()
+#Word counts before removing stopwords
+def word_count_plot(spamham, title):
+    # finding words along with count
+    word_counter = collections.Counter([word for sentence in spamham for word in sentence.split()])
+    most_count = word_counter.most_common(30)  # 30 most common words
+    # sorted data frame
+    most_count = pd.DataFrame(most_count, columns=["Words", "Occurance"]).sort_values(by="Occurance", ascending=False)
+    fig = px.bar(most_count, x="Words", y="Occurance", color="Occurance", template='ggplot2', title=title)
+    fig.show()
 
-# def word_count_plot(spamham, title):
-#     # finding words along with count
-#     word_counter = collections.Counter([word for sentence in spamham for word in sentence.split()])
-#     most_count = word_counter.most_common(30)  # 30 most common words
-#     # sorted data frame
-#     most_count = pd.DataFrame(most_count, columns=["Words", "Occurance"]).sort_values(by="Occurance", ascending=False)
-#     fig = px.bar(most_count, x="Words", y="Occurance", color="Occurance", template='ggplot2', title=title)
-#     fig.show()
-#
-# word_count_plot(spamham_data["messages"], "Word Count Before Removing StopWords")
+word_count_plot(spamham_data["messages"], "Word Count Before Removing StopWords")
 
 #removing stopwords
 def msg_process(msg):
@@ -111,6 +121,7 @@ remove_extra_stopwords = ['u', 'im', '2', 'ur', 'ill', '4', 'lor', 'r', 'n', 'da
 spamham_data['removing_extra_stopwords'] = spamham_data['removed_stopwords'].apply(lambda msg: [word for word in msg if word not in remove_extra_stopwords])
 print(spamham_data.head(5))
 
+# Joining Cleaning text and adding new Length to the table
 def get_final_text(msg):
     final_text=" ".join([word for word in msg])
     return final_text
@@ -118,32 +129,71 @@ spamham_data['final_text']=spamham_data['removing_extra_stopwords'].apply(lambda
 spamham_data['clean_length'] =spamham_data.final_text.str.len()
 print(spamham_data.head(5))
 
-# word_count_plot(spamham_data["final_text"],"Word Count After Removing StopWords")
+#Word Count After removing StopWords and Extra StopWords
+word_count_plot(spamham_data["final_text"],"Word Count After Removing StopWords")
 
-#WordCloud
-# ham_cloud = list(spamham_data.loc[spamham_data.catergories == 'ham', 'final_text'])
-#
-# wordcloud_ham = WordCloud(width = 500,
-#                      height = 500,
-#                      background_color ='white', min_font_size = 9).generate(' '.join(ham_cloud))
-# plt.figure(figsize = (10, 9),dpi=50, facecolor = None)
-# plt.imshow(wordcloud_ham)
-# plt.axis("off")
-# plt.title('WordCloud for Ham message')
-# plt.tight_layout(pad = 0)
-# plt.show()
+#Total Words remove after removing StopWords and Extra StopWords
+print("Original Length:",spamham_data.Length.sum())
+print("Cleaned Length:",spamham_data.clean_length.sum())
+print("Total Words Removed:",(spamham_data.Length.sum()) - (spamham_data.clean_length.sum()))
 
-# spam_cloud = list(spamham_data.loc[spamham_data.catergories == 'spam', 'final_text'])
-#
-# wordcloud_spam = WordCloud(width = 800, height = 800,
-#                      background_color ='white', min_font_size = 9).generate(' '.join(spam_cloud))
-# plt.figure(figsize = (10, 9),dpi=55, facecolor = None)
-# plt.imshow(wordcloud_spam)
-# plt.axis("off")
-# plt.title('WordCloud for Spam message')
-# plt.tight_layout(pad = 0)
-# plt.show()
+ham_words = list(spamham_data.loc[spamham_data.catergories == 'ham', 'removing_extra_stopwords'])
+ham_words = list(np.concatenate(ham_words).flat)
+ham_words = Counter(ham_words)
+ham_words = pd.DataFrame(ham_words.most_common(30), columns = ['word', 'frequency'])
 
+#WordCloud used in HAM messages
+ham_cloud = list(spamham_data.loc[spamham_data.catergories == 'ham', 'final_text'])
+
+wordcloud_ham = WordCloud(width = 500,
+                     height = 500,
+                     background_color ='white', min_font_size = 9).generate(' '.join(ham_cloud))
+plt.figure(figsize = (10, 9),dpi=50, facecolor = None)
+plt.imshow(wordcloud_ham)
+plt.axis("off")
+plt.title('WordCloud for Ham message')
+plt.tight_layout(pad = 0)
+plt.show()
+
+#Top 25 words in HAM messages
+ham_data_top25 = ham_words.sort_values(by='frequency', ascending=False)
+ham_data_top25 = ham_data_top25.head(25)
+
+plt.figure(figsize=(15,10))
+sns.barplot(x=ham_data_top25['word'], y=ham_data_top25['frequency'])
+plt.title("Words in HAM messages")
+plt.xticks(rotation=30)
+plt.show()
+
+
+spam_words = list(spamham_data.loc[spamham_data.catergories == 'spam', 'removing_extra_stopwords'])
+spam_words = list(np.concatenate(spam_words).flat)
+spam_words = Counter(spam_words)
+spam_words = pd.DataFrame(spam_words.most_common(30), columns = ['word', 'frequency'])
+
+# Word Cloud in SPAM messages
+spam_cloud = list(spamham_data.loc[spamham_data.catergories == 'spam', 'final_text'])
+
+wordcloud_spam = WordCloud(width = 800, height = 800,
+                     background_color ='white', min_font_size = 9).generate(' '.join(spam_cloud))
+plt.figure(figsize = (10, 9),dpi=55, facecolor = None)
+plt.imshow(wordcloud_spam)
+plt.axis("off")
+plt.title('WordCloud for Spam message')
+plt.tight_layout(pad = 0)
+plt.show()
+
+#Top25 words used in SPAM messages
+spam_data_top25 = spam_words.sort_values(by='frequency', ascending=False)
+spam_data_top25 = spam_data_top25.head(25)
+
+plt.figure(figsize=(15,10))
+sns.barplot(x=spam_data_top25['word'], y=spam_data_top25['frequency'])
+plt.title("Words in SPAM messages")
+plt.xticks(rotation=30)
+plt.show()
+
+#Replace "ham" to 0 and SPAM to 1
 spamham_data = spamham_data.replace(['ham','spam'],[0, 1])
 print(spamham_data.head(5))
 
